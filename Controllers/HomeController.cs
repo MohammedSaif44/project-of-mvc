f21825aac66138d3dd40c3fe818438a7c3ff8c49
MohamedSaif44 <mohasaif421@gmail.com>
2024-09-09 21:19:29 +0300

    Creation of session1

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace project_of_mvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return RedirectToAction("About Us");
+        }
+        public string AboutUs()
+        {
+            return "this is About Us";
+        }
+    }
+}
